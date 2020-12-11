b718b795ddc827b9e8281995964e470846d64090
baozi <shanll@bayread.com>
2020-12-11 10:47:37 +0800

    feat: init

--- App.vue
+++ App.vue
@@ -1,21 +1,81 @@
 <script>
-export default {
-	onLaunch: function() {
-		console.log('App Launch');
-	},
-	onShow: function() {
-		console.log('App Show');
-	},
-	onHide: function() {
-		console.log('App Hide');
-	},
-	globalData: {
-		api: {
-			home: 'https://055cfd20-bfe4-4b9a-be9d-f7c2cac59a57.bspapp.com/http/api/home',
+	export default {
+		onLaunch: function() {
+			console.log('App Launch');
 		},
-	}
+		onShow: function() {
+			console.log('App Show');
+		},
+		onHide: function() {
+			console.log('App Hide');
+		},
+		globalData: {
+			data: {
+				home: {
+					"tabs": [{
+						"_id": "5fc4bf91fb0f85000174c4c9",
+						"icon": "/static/all.png",
+						"tabId": 0,
+						"text": "全部"
+					}, {
+						"_id": "5fc4bf91fb0f85000174c4ca",
+						"icon": "/static/ele.png",
+						"tabId": 1,
+						"text": "饿了么"
+					}, {
+						"_id": "5fc4bf91fb0f85000174c4cb",
+						"icon": "/static/meituan.png",
+						"tabId": 2,
+						"text": "美团"
+					}, {
+						"_id": "5fc4bf91fb0f85000174c434",
+						"icon": "/static/pdd_icon.png",
+						"tabId": 5,
+						"text": "拼多多优惠券",
+						"appId": "wxe79b2252872fd7a4",
+						"path": "pages/index/index"
+					}],
+					"coupons": [{
+						"_id": "5fc4bf8ffbd6de00010be375",
+						"bannerPic": "/static/coupon/ele_banner.png",
+						"icon": "/static/coupon/ele.png",
+						"minapp": {
+							"appid": "wxece3a9a4c82f58c9",
+							"path": "pages/sharePid/web/index?scene=https://s.click.ele.me/INk7htu"
+						},
+						"name": "饿了么红包",
+						"tabId": 1,
+						"type": 1,
+						"url": "https://s.click.ele.me/INk7htu"
+					}, {
+						"_id": "5fc4bf8ffbd6de00010be376",
+						"bannerPic": "/static/coupon/meituan_banner.png",
+						"icon": "/static/coupon/meituan.png",
+						"minapp": {
+							"appid": "wx2c348cf579062e56",
+							"path": "outer_packages/r2xinvite/coupon/coupon?inviteCode=NnOIp-QOs8SiYF1dcSlL5r8phPrCf6qkH7evMyjIoup2NXxNCLYcBbd3bqpv2X2IztI9wuZn13xnLXxvn8WLwvS9nd9A-rgRplYmnh8VHpdw2lsrKipLUX7Yd8kB_0ETeNnH_DULAj1PCPbSVKBeTytyPPsDMFNQRr-aXA6evvc"
+						},
+						"name": "美团外卖红包",
+						"tabId": 2,
+						"type": 1
+					},{
+						"_id": "5fc4bf8ffbd6de00010be377",
+						"bannerPic": "/static/coupon/ele_fruit_banner.png",
+						"icon": "/static/coupon/ele.png",
+						"minapp": {
+							"appid": "wxece3a9a4c82f58c9",
+							"path": "pages/sharePid/web/index?scene=https://s.click.ele.me/8av5htu"
+						},
+						"name": "饿了么水果超市",
+						"tabId": 1,
+						"type": 1,
+						"url": "https://s.click.ele.me/8av5htu"
+					}]
+				},
+			},
+		}
 
-};
+	};
 </script>
 
 <style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -14,212 +14,233 @@
 					</view>
 					<view class="right">免费领取</view>
 				</view>
-				<view class="bottom"><image :src="v.bannerPic" mode="widthFix" /></view>
+				<view class="bottom">
+					<image :src="v.bannerPic" mode="widthFix" />
+				</view>
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			current: 0,
-			tabs: [],
-			couponList: [],
-			coupons: []
-		};
-	},
-	onLoad(e) {
-		this.getHome()
-		//#ifdef H5
-		let tabId = this.$route.query.tabId ? parseInt(this.$route.query.tabId) : 0
-		//#endif
-		//#ifdef MP-WEIXIN
-		let tabId = e.tabId ? parseInt(e.tabId) : 0
-		//#endif
-		for(let i in this.tabs){
-			if(tabId == this.tabs[i].tabId){
-				this.current = parseInt(i)
-			}
-		}
-		this.changeTab(this.current)
-	},
-	onShareAppMessage(res) {
-		var messages = [{
-			title: '美团饿了么大额红包，每日可领！',
-			path: '/pages/index/index'
-		},{
-			title: '吃了这么多年外卖，你知道这个秘密吗？',
-			path: '/pages/index/index'
-		},{
-			title: '这样点外卖，一年省下一个亿',
-			path: '/pages/index/index'
-		},{
-			title: '点外卖前先领券，吃霸王餐',
-			path: '/pages/index/index'
-		},{
-			title: '美团饿了么内部优惠券，手慢无',
-			path: '/pages/index/index'
-		},{
-			title: '点外卖不用优惠券，你就out了',
-			path: '/pages/index/index'
-		},{
-			title: '外卖不为人知的秘密，点这解密',
-			path: '/pages/index/index'
-		},{
-			title: '震惊！小伙点外卖竟然花了1分钱',
-			path: '/pages/index/index'
-		},{
-			title: '从这点外卖，你也可以吃霸王餐',
-			path: '/pages/index/index'
-		}];
-		return messages[Math.floor(Math.random()*messages.length)];
-	},
-	methods: {
-		changeTab(index) {
-			console.log('当前选中的项：' + index);
-			this.couponList = []
-			uni.showLoading({
-			    title: '获取优惠中'
-			});
-			if(index == 0){
-				this.couponList = this.coupons
-			}else{
-				for(let i in this.coupons){
-					if(this.coupons[i].tabId == this.tabs[index].tabId){
-						this.couponList.push(this.coupons[i])
-					}
+	export default {
+		data() {
+			return {
+				current: 0,
+				tabs: [],
+				couponList: [],
+				coupons: []
+			};
+		},
+		onLoad(e) {
+			this.getHome()
+			//#ifdef H5
+			let tabId = this.$route.query.tabId ? parseInt(this.$route.query.tabId) : 0
+			//#endif
+			//#ifdef MP-WEIXIN
+			let tabId = e.tabId ? parseInt(e.tabId) : 0
+			//#endif
+			for (let i in this.tabs) {
+				if (tabId == this.tabs[i].tabId) {
+					this.current = parseInt(i)
 				}
 			}
-			//#ifdef H5
-			this.$nextTick(() => {
-				this.$refs.coupon.scrollTop= 0;
-			})
-			//#endif
-			setTimeout(() => {
-				uni.hideLoading()
-			}, 500)
+			this.changeTab(this.current)
+
 		},
-		toCoupon(i){
-			console.log(this.couponList[i])
-			//h5
-			//#ifdef H5
-			window.location.href = this.couponList[i].url
-			//#endif
-			//微信小程序
-			//#ifdef MP-WEIXIN
-			if(this.couponList[i].minapp){
-				wx.navigateToMiniProgram({
-				  appId: this.couponList[i].minapp.appid,
-				  path: this.couponList[i].minapp.path,
-				  success(res) {
-					// 打开成功
-				  }
+		onShareAppMessage(res) {
+			var messages = [{
+				title: '美团饿了么大额红包，每日可领！',
+				path: '/pages/index/index'
+			}, {
+				title: '吃了这么多年外卖，你知道这个秘密吗？',
+				path: '/pages/index/index'
+			}, {
+				title: '这样点外卖，一年省下一个亿',
+				path: '/pages/index/index'
+			}, {
+				title: '点外卖前先领券，吃霸王餐',
+				path: '/pages/index/index'
+			}, {
+				title: '美团饿了么内部优惠券，手慢无',
+				path: '/pages/index/index'
+			}, {
+				title: '点外卖不用优惠券，你就out了',
+				path: '/pages/index/index'
+			}, {
+				title: '外卖不为人知的秘密，点这解密',
+				path: '/pages/index/index'
+			}, {
+				title: '震惊！小伙点外卖竟然花了1分钱',
+				path: '/pages/index/index'
+			}, {
+				title: '从这点外卖，你也可以吃霸王餐',
+				path: '/pages/index/index'
+			}];
+			return messages[Math.floor(Math.random() * messages.length)];
+		},
+		methods: {
+			changeTab(index) {
+				console.log('当前选中的项：' + index);
+				this.couponList = []
+				uni.showLoading({
+					title: '获取优惠中'
+				});
+				if (index == 0) {
+					this.couponList = this.coupons
+				} else {
+					for (let i in this.coupons) {
+						if (this.coupons[i].tabId == this.tabs[index].tabId) {
+							this.couponList.push(this.coupons[i])
+						}
+					}
+				}
+				//#ifdef H5
+				this.$nextTick(() => {
+					this.$refs.coupon.scrollTop = 0;
 				})
+				//#endif
+				setTimeout(() => {
+					uni.hideLoading()
+				}, 500)
+			},
+			toCoupon(i) {
+				wx.requestSubscribeMessage({
+					tmplIds: ['roQJZa0Ih9Mg_FL2ND_r0LI1MOTgO1tQW117aOz_Urc'],
+					success(res) {
+						console.log(res)
+					},
+					fail(err) {
+						console.log(err)
+					}
+				})
+				console.log(this.couponList[i])
+				//h5
+				//#ifdef H5
+				window.location.href = this.couponList[i].url
+				//#endif
+				//微信小程序
+				//#ifdef MP-WEIXIN
+				if (this.couponList[i].minapp) {
+					wx.navigateToMiniProgram({
+						appId: this.couponList[i].minapp.appid,
+						path: this.couponList[i].minapp.path,
+						success(res) {
+							// 打开成功
+						}
+					})
+				}
+				//#endif
+			},
+			getHome() {
+				console.log(getApp().globalData.data.home)
+				let data = getApp().globalData.data.home
+				this.tabs = data.tabs
+				this.coupons = data.coupons
+				this.changeTab(0)
+
 			}
-			//#endif
-		},
-		getHome(){
-			uni.request({
-			    url: getApp().globalData.api.home,
-			    success: (res) => {
-			        this.tabs = res.data.data.tabs
-					this.coupons = res.data.data.coupons
-					this.changeTab(0)
-			    }
-			});
 		}
-	}
-};
+	};
 </script>
 
 <style lang="scss">
-page {
-	background-color: #f8f8f8;
-}
-.container {
-	font-size: 14px;
-	line-height: 24px;
-	position: relative;
-	.tab {
-		position: fixed;
-		top: var(--window-top);
-		left: 0;
-		z-index: 9999;
+	page {
+		background-color: #f8f8f8;
 	}
-	.coupon {
-		padding-top: 200rpx;
-		padding-bottom: 10rpx;
-		.item {
-			background-color: #ffffff;
-			margin: 30rpx;
-			border-radius: 10rpx;
-			padding: 0 30rpx 30rpx 30rpx;
-			.top {
-				height: 116rpx;
-				display: flex;
-				align-items: center;
-				justify-content: space-between;
-				.left {
+
+	.container {
+		font-size: 14px;
+		line-height: 24px;
+		position: relative;
+
+		.tab {
+			position: fixed;
+			top: var(--window-top);
+			left: 0;
+			z-index: 9999;
+		}
+
+		.coupon {
+			padding-top: 200rpx;
+			padding-bottom: 10rpx;
+
+			.item {
+				background-color: #ffffff;
+				margin: 30rpx;
+				border-radius: 10rpx;
+				padding: 0 30rpx 30rpx 30rpx;
+
+				.top {
 					height: 116rpx;
-					width: 400rpx;
 					display: flex;
 					align-items: center;
 					justify-content: space-between;
-					.content {
-						width: 100%;
+
+					.left {
+						height: 116rpx;
+
+						display: flex;
+						align-items: center;
+						justify-content: space-between;
+
+						.content {
+							width: 100%;
+						}
+
+						.icon {
+							display: inline-block;
+							vertical-align: bottom;
+							width: 52rpx;
+							height: auto;
+						}
+
+						.name {
+							text-align: left;
+							display: inline-block;
+							vertical-align: bottom;
+							font-size: 34rpx;
+							color: #000;
+							line-height: 50rpx;
+							font-weight: bold;
+							margin-left: 15rpx;
+						}
+
+						.text {
+							width: 150rpx;
+							height: 38rpx;
+							line-height: 38rpx;
+							text-align: center;
+							font-size: 24rpx;
+							color: #61300e;
+							background: linear-gradient(90deg, #f9db8d, #f8d98a);
+							border-radius: 6rpx;
+						}
 					}
-					.icon {
-						display: inline-block;
-						vertical-align: bottom;
-						width: 52rpx;
+
+					.right {
+						width: 170rpx;
+						height: 60rpx;
+						border-radius: 30rpx;
+						background: linear-gradient(90deg, #ec6f43, #ea4a36);
+						color: #fff;
+						font-size: 28rpx;
+						line-height: 60rpx;
+						text-align: center;
+					}
+				}
+
+				.bottom {
+					height: auto;
+					width: 100%;
+
+					image {
+						display: block;
+						width: 100%;
 						height: auto;
 					}
-					.name {
-						text-align: left;
-						display: inline-block;
-						vertical-align: bottom;
-						font-size: 34rpx;
-						color: #000;
-						line-height: 50rpx;
-						font-weight: bold;
-						margin-left: 15rpx;
-					}
-					.text {
-						width: 150rpx;
-						height: 38rpx;
-						line-height: 38rpx;
-						text-align: center;
-						font-size: 24rpx;
-						color: #61300e;
-						background: linear-gradient(90deg, #f9db8d, #f8d98a);
-						border-radius: 6rpx;
-					}
-				}
-
-				.right {
-					width: 170rpx;
-					height: 60rpx;
-					border-radius: 30rpx;
-					background: linear-gradient(90deg, #ec6f43, #ea4a36);
-					color: #fff;
-					font-size: 28rpx;
-					line-height: 60rpx;
-					text-align: center;
-				}
-			}
-
-			.bottom {
-				height: auto;
-				width: 100%;
-				image {
-					display: block;
-					width: 100%;
-					height: auto;
 				}
 			}
 		}
 	}
-}
 </style>
